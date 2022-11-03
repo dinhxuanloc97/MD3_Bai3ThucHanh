be0fab986292cdb2f79fea0a0cae4bfd02bd4c28
dinhxuanloc97 <112063673+dinhxuanloc97@users.noreply.github.com>
2022-11-03 17:00:30 +0700

    Add files via upload

---
+++ B3Th2.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE b3Th2QuanLySinhVien;
+USE b3Th2QuanLySinhVien;
+CREATE TABLE Class
+(
+    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL,
+    StartDate DATETIME    NOT NULL,
+    Status    BIT
+);
+CREATE TABLE Student
+(
+    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address     VARCHAR(50),
+    Phone       VARCHAR(20),
+    Status      BIT,
+    ClassId     INT         NOT NULL,
+    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
+);
+CREATE TABLE Subject
+(
+    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL,
+    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    Status  BIT                  DEFAULT 1
+);
+
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+SELECT *
+FROM Student;
+
+#Hiển thị danh sách học viên đang theo học
+SELECT *
+FROM Student
+WHERE Status = true;
+
+#Hiển thị danh sách các môn học có thời gian học nhỏ hơn 10 giờ.
+SELECT *
+FROM Subject
+WHERE Credit < 10;
+
+#Hiển thị danh sách học viên lớp A1
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID
+WHERE C.ClassName = 'A1';
+
+#Hiển thị điểm môn CF của các học viên.
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
